c482579febda60ba566a4b103402413869a74bc9
amir jaberi <a.m.amirjaberiii@gmail.com>
2025-01-06 19:59:57 +0330

    1403/10/17  <5>

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+config = {'user': os.environ['user'], 'password': os.environ['password'], 'host': 'localhost', 'database': 'jaberi_exchange'}
+
+token = os.environ['token']
+
+admin = os.environ['admin']
+
+bank_card_number = os.environ['bank_card_number']
+
+bank_card_photo = os.environ['bank_card_photo']
